33ccce18de8344ed4ca9eebc927c5ec0d806fff7
Mahmoud Bekheet <mahmoud.a.bekheet@gmail.com>
2024-05-06 08:07:56 +0300

    c++: add STL summary

---
+++ c++/17-STL/0.summary.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+/*
+
+  ==> Quick Summary for STL
+  Queue:
+    - [FIFO] Add/Remove from the end only
+  Deque:
+   - Add/Remove from the front & the end
+  Stack:
+   - LIFO: Pop from the top
+  Vector:
+    - Flexible size DS.
+    - Push to back & Pop from the back.
+    - Erasion can be done by Iterators
+  Map:
+    - Key-value pairs (Not zero-based as regular arrays)
+  Set:
+    - Ordered & Unique.
+  Multiset:
+   - Ordered & NOT Unique
+  Unordered Set:
+  - Unordered & Unique
+  Pair:
+    - A pair of 2 values only that can be of any datatype.
+    - Comparison between pairs is done from the left to right.
+  Tuple:
+   - An array of different types. Like a group of pairs
+  Iterator:
+  - An object looking at an index of the Data Structure that helps with looping over different DS.
+  - Iterator begin() is at 0 & Iterator end() is after @size() of the DS (1-based unlike the index).
+  - rbegin() and rend() iterate in the reverse order
+*/
+
+int main()
+{
+}
